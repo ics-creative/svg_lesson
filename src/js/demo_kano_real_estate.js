d307b4cdc31a1832d82716bd23b8c2060aa01bc1
tonkotsuboy_com <t.kano.624@gmail.com>
2017-06-15 03:18:52 +0900

    refoctring

--- src/js/demo_kano_real_estate.js
+++ src/js/demo_kano_real_estate.js
@@ -1,20 +1,26 @@
+// 町のラインアート
 new Vivus("city", {duration: 120}, function () {
+    // city内のpathの塗りの透明度を1にする
     Snap("#city").selectAll("path").animate({
         "fill-opacity": 1
     }, 120)
 });
 
+// 地図のピンを上部に固定しておく
+Snap("#mapPin").attr({
+    transform:"translate(0, -400px)"
+});
+
+// 地図のラインアート
 new Vivus("map", {duration: 60}, function () {
+    // map内のpathの塗りの透明度を1にする
     Snap("#map").selectAll("path").animate({
         "fill-opacity": 1
-    }, 60, function() {
-    });
+    }, 60);
 
+    // 地図のピンを下ろす
     Snap("#mapPin").animate({
         transform:"translate(0, 0px)"
     }, 120, mina.easeout);
 });
 
-Snap("#mapPin").attr({
-    transform:"translate(0, -400px)"
-});
